60add9f99023cb518907854d143ecdda667048e7
董向明 <dongxiangming@xuejj.com>
2024-04-02 09:21:49 +0800

    支持tailwindcss

--- src/views/userChildOne.vue
+++ src/views/userChildOne.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>这是第一个子路由</div>
+  <div class="text-cyan-400 font-bold text-3xl decoration-8">这是第一个子路由</div>
 </template>
 
 <style scoped></style>
