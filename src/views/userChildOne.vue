19eb4794504ced7588e8cfb763ad9dc7cf7455f9
董向明 <dongxiangming@xuejj.com>
2024-04-02 18:04:01 +0800

    tailwindcss示例

--- src/views/userChildOne.vue
+++ src/views/userChildOne.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="text-cyan-400 font-bold text-3xl decoration-8">这是第一个子路由</div>
+  <div>
+    <div class="grid grid-cols-4 gap-4 p-5">
+      <div class="col-start-2 col-end-4 col-span-2 bg-green-400 text-5xl antialiasing overline rounded-2xl">1</div>
+      <div class="w-20 h-20 bg-violet-700 text-cyan-50 text-center underline rounded-2xl">2</div>
+      <div class="w-20 h-20 bg-violet-700 text-cyan-50 text-center line-through decoration-amber-500">3</div>
+      <div class="w-20 h-20 bg-violet-700 text-cyan-50 text-center align-middle">4</div>
+      <div class="w-20 h-20 bg-violet-700 text-cyan-50 text-center">5</div>
+      <div class="w-20 h-20 bg-violet-700 text-cyan-50 text-center">6</div>
+      <div class="w-20 h-20 bg-violet-700 text-cyan-50 text-center">7</div>
+      <div class="w-20 h-20 bg-violet-700 text-cyan-50 text-center">8</div>
+      <div class="w-20 h-20 bg-violet-700 text-cyan-50 text-center">9</div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
